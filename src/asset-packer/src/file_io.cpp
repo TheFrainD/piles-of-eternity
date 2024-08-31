be7355e021d147c8c2b03cccfcfe5eb7ec6f3c9c
Dmytro Zykov <dmytri.zykov@gmail.com>
2024-08-31 19:13:17 +0300

    Add iterative file search

--- src/asset-packer/src/file_io.cpp
+++ src/asset-packer/src/file_io.cpp
@@ -3,8 +3,9 @@
 #include <fstream>
 
 namespace file_io {
-std::optional<std::vector<char>> load_file(
-    const std::filesystem::path &path) noexcept {
+namespace fs = std::filesystem;
+
+std::optional<std::vector<char>> load_file(const fs::path &path) noexcept {
     std::ifstream file(path);
     if (!file.is_open()) {
         return std::nullopt;
@@ -13,5 +14,34 @@ std::optional<std::vector<char>> load_file(
     std::vector<char> buffer((std::istreambuf_iterator<char>(file)),
                              std::istreambuf_iterator<char>());
     return buffer;
+}
+
+std::variant<std::vector<fs::path>, std::string_view> find_files_with_extension(
+    const fs::path &directory, const std::string_view &extension,
+    bool recursive) noexcept {
+    std::vector<fs::path> files;
+
+    auto process_entry = [&files, extension](const auto &entry) {
+        if (entry.is_regular_file() && entry.path().extension() == extension) {
+            files.push_back(entry.path());
+        }
+    };
+
+    try {
+        if (recursive) {
+            for (const auto &entry :
+                 fs::recursive_directory_iterator(directory)) {
+                process_entry(entry);
+            }
+            return files;
+        }
+
+        for (const auto &entry : fs::directory_iterator(directory)) {
+            process_entry(entry);
+        }
+        return files;
+    } catch (const fs::filesystem_error &err) {
+        return err.what();
+    }
 }
 }  // namespace file_io
--- src/asset-packer/src/main.cpp
+++ src/asset-packer/src/main.cpp
@@ -1,35 +1,32 @@
 #include <spdlog/spdlog.h>
 
+#include <iostream>
+
 #include "compression.h"
 #include "file_io.h"
 
 int main(int argc, char *argv[]) {
     using namespace std::literals;
+    namespace fs = std::filesystem;
 
     if (argc != 2) {
         spdlog::error("Requires only one arguments. {} given."sv, argc - 1);
         return 1;
     }
 
-    std::filesystem::path filepath(argv[1]);
+    fs::path directory_path(argv[1]);
 
-    auto buf = file_io::load_file(filepath);
-    if (!buf.has_value()) {
-        spdlog::error("Failed to load file at {}"sv, filepath.string());
-        return 1;
-    }
-
-    auto result = comp::compress_buffer(buf.value());
+    auto result = file_io::find_files_with_extension(directory_path, ".json");
     if (std::holds_alternative<std::string_view>(result)) {
-        spdlog::error("Compression failed! Error: {}",
+        spdlog::error("Failed to scan directory! Error: {}"sv,
                       std::get<std::string_view>(result));
         return 1;
     }
 
-    auto compressed_buf = std::get<std::vector<char>>(result);
-
-    spdlog::info("Success! Original size: {}\tCompressed size: {}"sv,
-                 buf->size(), compressed_buf.size());
+    auto files = std::get<std::vector<fs::path>>(result);
+    for (const auto &filepath : files) {
+        std::cout << filepath << '\n';
+    }
 
     return 0;
 }
